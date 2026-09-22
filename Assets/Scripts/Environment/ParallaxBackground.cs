ab9136e6f45f267ffbe68e12672414644a509912
Nicolas Mironoff <156594752+nicomirr@users.noreply.github.com>
2026-09-22 13:49:30 -0300

    Parallax system reworked

--- Assets/Scripts/Environment/ParallaxBackground.cs
+++ Assets/Scripts/Environment/ParallaxBackground.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
-using System.Collections.Generic;
-using UnityEngine.Tilemaps;
 using Game.Data;
 using Game.Events;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Environment
 {
     public class ParallaxBackground : MonoBehaviour
     {
         [SerializeField] private ParallaxDataSo _data;
+
         [SerializeField] private List<Transform> _backgrounds = new();
 
         [SerializeField] private MonoBehaviour _worldSpeedProviderMonobehaviour;
 
-        [SerializeField] private float _seamOverlap = 1f / 64f;
-
         private ISpeedProvider _worldSpeedProvider;
+
+        private float _backgroundWidth;
 
         private bool _isWorking;
 
@@ -23,8 +23,17 @@ namespace Game.Environment
         {
             _worldSpeedProvider = _worldSpeedProviderMonobehaviour as ISpeedProvider;
 
-            CompressTilemapBounds();
-            PositionBackgrounds();
+            SpriteRenderer spriteRenderer = _backgrounds[0].GetComponent<SpriteRenderer>();
+
+            _backgroundWidth = spriteRenderer.bounds.size.x;
+
+            for (int i = 1; i < _backgrounds.Count; i++)
+            {
+                Vector3 position = _backgrounds[0].position;
+                position.x += _backgroundWidth * i;
+
+                _backgrounds[i].position = position;
+            }
         }
 
         private void OnEnable()
@@ -55,59 +64,12 @@ namespace Game.Environment
             _isWorking = false;
         }
 
-        private void CompressTilemapBounds()
-        {
-            foreach (Transform background in _backgrounds)
-            {
-                Tilemap tilemap = background.GetComponentInChildren<Tilemap>();
-                tilemap.CompressBounds();
-            }
-        }
-
-        private void PositionBackgrounds()
-        {
-            for (int i = 1; i < _backgrounds.Count; i++)
-            {
-                Tilemap previousTilemap =
-                    _backgrounds[i - 1].GetComponentInChildren<Tilemap>();
-
-                Tilemap currentTilemap =
-                    _backgrounds[i].GetComponentInChildren<Tilemap>();
-
-                float previousRightEdge = GetRightEdge(previousTilemap);
-                float currentLeftEdge = GetLeftEdge(currentTilemap);
-
-                float difference =
-                    previousRightEdge - currentLeftEdge - _seamOverlap;
-
-                _backgrounds[i].position += Vector3.right * difference;
-            }
-        }
-
-        private float GetLeftEdge(Tilemap tilemap)
-        {
-            BoundsInt bounds = tilemap.cellBounds;
-
-            return tilemap.CellToWorld(
-                new Vector3Int(bounds.xMin, bounds.yMin, 0)).x;
-        }
-
-        private float GetRightEdge(Tilemap tilemap)
-        {
-            BoundsInt bounds = tilemap.cellBounds;
-
-            return tilemap.CellToWorld(
-                new Vector3Int(bounds.xMax, bounds.yMin, 0)).x;
-        }
-
         private void MoveBackgrounds()
         {
             foreach (Transform background in _backgrounds)
             {
-                background.position += Vector3.left *
-                    (_worldSpeedProvider.WorldCurrentSpeed *
-                     _data.SpeedModifierData.SpeedModifier *
-                     Time.deltaTime);
+                background.position += Vector3.left * (_worldSpeedProvider.WorldCurrentSpeed * 
+                    _data.SpeedModifierData.SpeedModifier * Time.deltaTime);
             }
         }
 
@@ -115,45 +77,15 @@ namespace Game.Environment
         {
             foreach (Transform background in _backgrounds)
             {
-                if (background.position.x > _data.MinXPos)
-                    continue;
-
-                Transform rightmostBackground = GetRightmostBackground(background);
-
-                Tilemap currentTilemap =
-                    background.GetComponentInChildren<Tilemap>();
-
-                Tilemap rightmostTilemap =
-                    rightmostBackground.GetComponentInChildren<Tilemap>();
-
-                float currentLeftEdge = GetLeftEdge(currentTilemap);
-                float rightmostRightEdge = GetRightEdge(rightmostTilemap);
-
-                float difference =
-                    rightmostRightEdge - currentLeftEdge - _seamOverlap;
-
-                background.position += Vector3.right * difference;
-            }
-        }
-
-        private Transform GetRightmostBackground(Transform excludedBackground)
-        {
-            Transform rightmost = null;
-
-            foreach (Transform background in _backgrounds)
-            {
-                if (background == excludedBackground)
-                    continue;
-
-                if (rightmost == null ||
-                    GetRightEdge(background.GetComponentInChildren<Tilemap>()) >
-                    GetRightEdge(rightmost.GetComponentInChildren<Tilemap>()))
+                if (background.position.x <= _data.MinXPos)
                 {
-                    rightmost = background;
+                    Vector3 position = background.position;
+
+                    position.x += _backgroundWidth * _backgrounds.Count;
+
+                    background.position = position;
                 }
             }
-
-            return rightmost;
         }
     }
 }
---
+++ Assets/Scripts/Environment/ParallaxBackgroundDELETE.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using Game.Data;
+using Game.Events;
+
+namespace Game.Environment
+{
+    public class ParallaxBackgroundDELETE : MonoBehaviour
+    {
+        [SerializeField] private ParallaxDataSo _data;
+        [SerializeField] private List<Transform> _backgrounds = new();
+
+        [SerializeField] private MonoBehaviour _worldSpeedProviderMonobehaviour;
+
+        [SerializeField] private float _seamOverlap = 1f / 64f;
+
+        private ISpeedProvider _worldSpeedProvider;
+
+        private bool _isWorking;
+
+        private void Awake()
+        {
+            _worldSpeedProvider = _worldSpeedProviderMonobehaviour as ISpeedProvider;
+
+            CompressTilemapBounds();
+            PositionBackgrounds();
+        }
+
+        private void OnEnable()
+        {
+            PlayerEvents.OnPlayerDeath += StopParallax;
+        }
+
+        private void Start()
+        {
+            _isWorking = true;
+        }
+
+        private void Update()
+        {
+            if (!_isWorking) return;
+
+            MoveBackgrounds();
+            RepositionBackgrounds();
+        }
+
+        private void OnDisable()
+        {
+            PlayerEvents.OnPlayerDeath -= StopParallax;
+        }
+
+        private void StopParallax()
+        {
+            _isWorking = false;
+        }
+
+        private void CompressTilemapBounds()
+        {
+            foreach (Transform background in _backgrounds)
+            {
+                Tilemap tilemap = background.GetComponentInChildren<Tilemap>();
+                tilemap.CompressBounds();
+            }
+        }
+
+        private void PositionBackgrounds()
+        {
+            for (int i = 1; i < _backgrounds.Count; i++)
+            {
+                Tilemap previousTilemap =
+                    _backgrounds[i - 1].GetComponentInChildren<Tilemap>();
+
+                Tilemap currentTilemap =
+                    _backgrounds[i].GetComponentInChildren<Tilemap>();
+
+                float previousRightEdge = GetRightEdge(previousTilemap);
+                float currentLeftEdge = GetLeftEdge(currentTilemap);
+
+                float difference =
+                    previousRightEdge - currentLeftEdge - _seamOverlap;
+
+                _backgrounds[i].position += Vector3.right * difference;
+            }
+        }
+
+        private float GetLeftEdge(Tilemap tilemap)
+        {
+            BoundsInt bounds = tilemap.cellBounds;
+
+            return tilemap.CellToWorld(
+                new Vector3Int(bounds.xMin, bounds.yMin, 0)).x;
+        }
+
+        private float GetRightEdge(Tilemap tilemap)
+        {
+            BoundsInt bounds = tilemap.cellBounds;
+
+            return tilemap.CellToWorld(
+                new Vector3Int(bounds.xMax, bounds.yMin, 0)).x;
+        }
+
+        private void MoveBackgrounds()
+        {
+            foreach (Transform background in _backgrounds)
+            {
+                background.position += Vector3.left *
+                    (_worldSpeedProvider.WorldCurrentSpeed *
+                     _data.SpeedModifierData.SpeedModifier *
+                     Time.deltaTime);
+            }
+        }
+
+        private void RepositionBackgrounds()
+        {
+            foreach (Transform background in _backgrounds)
+            {
+                if (background.position.x > _data.MinXPos)
+                    continue;
+
+                Transform rightmostBackground = GetRightmostBackground(background);
+
+                Tilemap currentTilemap =
+                    background.GetComponentInChildren<Tilemap>();
+
+                Tilemap rightmostTilemap =
+                    rightmostBackground.GetComponentInChildren<Tilemap>();
+
+                float currentLeftEdge = GetLeftEdge(currentTilemap);
+                float rightmostRightEdge = GetRightEdge(rightmostTilemap);
+
+                float difference =
+                    rightmostRightEdge - currentLeftEdge - _seamOverlap;
+
+                background.position += Vector3.right * difference;
+            }
+        }
+
+        private Transform GetRightmostBackground(Transform excludedBackground)
+        {
+            Transform rightmost = null;
+
+            foreach (Transform background in _backgrounds)
+            {
+                if (background == excludedBackground)
+                    continue;
+
+                if (rightmost == null ||
+                    GetRightEdge(background.GetComponentInChildren<Tilemap>()) >
+                    GetRightEdge(rightmost.GetComponentInChildren<Tilemap>()))
+                {
+                    rightmost = background;
+                }
+            }
+
+            return rightmost;
+        }
+    }
+}
